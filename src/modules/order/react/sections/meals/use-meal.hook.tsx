9b8a1a348c6787d780323cd663b13b13425d4c8d
benjamin GIRARD <benjamingirard25@gmail.com>
2024-05-02 11:50:20 +0200

    task: ui for mealSection

---
+++ src/modules/order/react/sections/meals/use-meal.hook.tsx
@@ -0,0 +1,50 @@
+import { OrderingDomainModel } from "@ratatouille/modules/order/core/model/ordering.domain-model";
+
+export const useMeal = () => {
+  function getSelectableEntries(guestId: string): OrderingDomainModel.Meal[] {
+    return [];
+  }
+  function getSelectableMainCourses(
+    guestId: string
+  ): OrderingDomainModel.Meal[] {
+    return [];
+  }
+  function getSelectableDesserts(guestId: string): OrderingDomainModel.Meal[] {
+    return [];
+  }
+  function getSelectableDrinks(guestId: string): OrderingDomainModel.Meal[] {
+    return [];
+  }
+
+  function assignEntry(guestId: string, mealId: string): void {}
+  function assignMainCourse(guestId: string, mealId: string): void {}
+  function assignDessert(guestId: string, mealId: string): void {}
+  function assignDrink(guestId: string, mealId: string): void {}
+
+  function onNext() {}
+  function onPrevious() {}
+
+  function isSubmittable() {
+    return false;
+  }
+
+  const guests: OrderingDomainModel.Guest[] = [];
+
+  return {
+    getSelectableEntries,
+    getSelectableMainCourses,
+    getSelectableDesserts,
+    getSelectableDrinks,
+
+    assignEntry,
+    assignMainCourse,
+    assignDessert,
+    assignDrink,
+
+    onNext,
+    onPrevious,
+
+    isSubmittable: isSubmittable(),
+    guests,
+  };
+};
